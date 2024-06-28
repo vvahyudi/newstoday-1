ebc082f20ce5276943a2f214d33ff1f20fecc2ff
Ahmad Wahyudi <ahmadwahyudi2395@gmail.com>
2024-06-28 13:26:52 +0700

    ft: fix detail article page

--- src/app/article/article-view/[id]/page.jsx
+++ src/app/article/article-view/[id]/page.jsx
@@ -13,7 +13,7 @@ import Footer from "@/components/organisms/Footer"
 import { useArticleByIdQuery } from "@/hooks/useArticleQuery"
 import { useDetailProfileQuery } from "@/hooks/useProfileQuery"
 import { useParams } from "next/navigation"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 // import { useRouter } from "next/navigation"
 import { useMutation } from "@tanstack/react-query"
 import { createComment } from "@/api/article"
@@ -27,6 +27,11 @@ export default function ArticleViewPage() {
 	const [form, setForm] = useState({
 		comment: "",
 	})
+	const [token, setToken] = useState(null)
+	useEffect(() => {
+		const getToken = localStorage.getItem("token")
+		setToken(getToken)
+	}, [])
 	const createCommentMutation = useMutation({
 		mutationFn: createComment,
 	})
@@ -61,30 +66,29 @@ export default function ArticleViewPage() {
 	return (
 		<>
 			{/* {JSON.stringify(data)} */}
+			<Navbar />
 			{isLoading ? (
 				<div className="flex items-center w-full justify-center">
 					<span className="loading loading-spinner loading-lg"></span>
 				</div>
 			) : (
-				<>
-					<Navbar />
-					<section className="flex flex-col justify-center p-8 w-full">
-						<ArticleHeader
-							sendIcon={
-								<Icon icon="bi:send" className="text-black w-6 h-6 font-bold" />
-							}
+				<section className="flex flex-col justify-center p-8 w-full">
+					<ArticleHeader
+						sendIcon={
+							<Icon icon="bi:send" className="text-black w-6 h-6 font-bold" />
+						}
+					/>
+					<CardImageArticle>
+						<Image
+							src={`${data.data.banner}`}
+							alt={`${data.data.title}`}
+							// src={`/bg-hero-article.png`}
+							// alt={"Image Article"}
+							width={480}
+							height={480}
+							className="object-cover max-w-screen-md md:max-w-screen-sm"
 						/>
-						<CardImageArticle>
-							<Image
-								src={`${data.data.banner}`}
-								alt={`${data.data.title}`}
-								// src={`/bg-hero-article.png`}
-								// alt={"Image Article"}
-								width={480}
-								height={480}
-								className="object-cover max-w-screen-md md:max-w-screen-sm"
-							/>
-							{/* <Image
+						{/* <Image
 							src={`${data.data.banner}`}
 							alt={`${data.data.title}`}
 							// src={`/bg-hero-article.png`}
@@ -93,21 +97,22 @@ export default function ArticleViewPage() {
 							height={500}
 							className="object-cover max-w-screen-sm"
 						/> */}
-						</CardImageArticle>
-						<SectionAvatar
-							picture={data.data.user.picture}
-							username={data.data.user.username}
-							job={data.data.user.job}
-							created_at={data.data.createdAt}
-						/>
-						<section className="flex flex-col w-full gap-4">
-							{/* {JSON.stringify(data)} */}
-							<h1 className={`text-xl md:text-4xl ${nunitoBold.className}`}>
-								{`${data.data.title}`}
-								{/* {"Image Article"} */}
-							</h1>
-							<p className="prose prose-xl text-sm md:text-lg">{`${data.data.body}`}</p>
-						</section>
+					</CardImageArticle>
+					<SectionAvatar
+						picture={data.data.user.picture}
+						username={data.data.user.username}
+						job={data.data.user.job}
+						created_at={data.data.createdAt}
+					/>
+					<section className="flex flex-col w-full gap-4">
+						{/* {JSON.stringify(data)} */}
+						<h1 className={`text-xl md:text-4xl ${nunitoBold.className}`}>
+							{`${data.data.title}`}
+							{/* {"Image Article"} */}
+						</h1>
+						<p className="prose prose-xl text-sm md:text-lg">{`${data.data.body}`}</p>
+					</section>
+					{token ? (
 						<section className="flex flex-col w-full gap-2 pt-8">
 							<h2 className={`text-2xl ${nunitoBold.className}`}>
 								{data.data.comment.length} Comments
@@ -157,20 +162,20 @@ export default function ArticleViewPage() {
 								</form>
 							</div>
 						</section>
+					) : null}
 
-						{data.data.comment.map((comment) => (
-							<SectionComment
-								key={comment.id}
-								articleOwnerAvatar={data.data.user.picture}
-								articleOwnerName={data.data.user.username}
-								commentatorAvatar={comment.user.picture}
-								commentatorName={comment.user.username}
-								commentatorDate={comment.createdAt}
-								commentContent={comment.comment}
-							/>
-						))}
-					</section>
-				</>
+					{data.data.comment.map((comment) => (
+						<SectionComment
+							key={comment.id}
+							articleOwnerAvatar={data.data.user.picture}
+							articleOwnerName={data.data.user.username}
+							commentatorAvatar={comment.user.picture}
+							commentatorName={comment.user.username}
+							commentatorDate={comment.createdAt}
+							commentContent={comment.comment}
+						/>
+					))}
+				</section>
 			)}
 
 			<Footer />
